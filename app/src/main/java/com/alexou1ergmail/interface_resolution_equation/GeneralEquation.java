cb0452c7c2749fc0fbed89726949a668616a7ed2
NeuralMath <marcantoinelafreniere@hotmail.com>
2017-05-01 13:19:08 -0400

    Initial commit

---
+++ app/src/main/java/com/alexou1ergmail/interface_resolution_equation/GeneralEquation.java
@@ -0,0 +1,17 @@
+package com.alexou1ergmail.interface_resolution_equation;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Alex on 29/03/2017.
+ */
+
+public class GeneralEquation {
+    protected String m_equation;
+
+    public GeneralEquation(String equation) {
+        this.m_equation = equation;
+    }
+
+
+}
